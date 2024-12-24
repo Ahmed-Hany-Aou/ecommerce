4126e7bdd31e80eff5a6c350b583b6a391a52158
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-12-24 09:20:48 +0200

    implemented update option v1 , without image handling

--- resources/views/backend/product/product_edit.blade.php
+++ resources/views/backend/product/product_edit.blade.php
@@ -21,8 +21,9 @@
 			  <div class="row">
 				<div class="col">
 
-  <form method="post" action="{{ route('product-store') }}" enctype="multipart/form-data" >
-		 	@csrf
+                    <form method="post" action="{{ route('product-update') }}" >
+                        @csrf
+                       <input type="hidden" name="id" value="{{ $products->id }}">
 
 					  <div class="row">
 	<div class="col-12">	
