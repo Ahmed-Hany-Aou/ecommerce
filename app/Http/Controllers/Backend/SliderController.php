2b5d9998653ffedfcc62fd786a80248bf79786db
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-12-27 20:15:13 +0200

    added update function

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Slider;
+use Image;
+use Carbon\Carbon;
 
 class SliderController extends Controller
 {
@@ -12,5 +14,29 @@ class SliderController extends Controller
 		$sliders = Slider::latest()->get();
 		return view('backend.slider.slider_view',compact('sliders'));
     }
+    public function SliderStore(Request $request){
+
+    	$request->validate([
+    		 
+    		'slider_img' => 'required',
+    	],[
+    		'slider_img.required' => 'Plz Select One Image',
+    		 
+    	]);
+    	$image = $request->file('slider_img');
+    	$name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+    	Image::make($image)->resize(870,370)->save('upload/slider/'.$name_gen);
+    	$save_url = 'upload/slider/'.$name_gen;
+	Slider::insert([
+		'title' => $request->title,
+		'description' => $request->description,
+		'slider_img' => $save_url,
+    	]);
+	    $notification = array(
+			'message' => 'Slider Inserted Successfully',
+			'alert-type' => 'success'
+		);
+		return redirect()->back()->with($notification);
+    } // end method 
 
 }
