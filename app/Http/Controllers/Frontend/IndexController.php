6519e17a2494f452a24a65516d4d808310e1bb95
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-12-29 23:13:02 +0200

    implemented special deals

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -21,7 +21,10 @@ class IndexController extends Controller
 		$featured = Product::where('featured',1)->orderBy('id','DESC')->limit(6)->get();
 		$hot_deals = Product::where('hot_deals',1)->orderBy('id','DESC')->limit(4)->get();
     	$categories = Category::orderBy('category_name_en','ASC')->get();
-    	return view('frontend.index',compact('categories','sliders','products','featured','hot_deals'));
+		$hot_deals = Product::where('hot_deals',1)->orderBy('id','DESC')->limit(3)->get();
+    	$special_offer = Product::where('special_offer',1)->orderBy('id','DESC')->limit(6)->get();
+		$special_deals = Product::where('special_deals',1)->orderBy('id','DESC')->limit(3)->get();
+    	return view('frontend.index',compact('categories','sliders','products','featured','hot_deals','special_offer','special_deals'));
 
     }
 
