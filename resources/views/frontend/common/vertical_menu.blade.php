cd0a596f70843beef9f51b9e1dc37cb437585bcb
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-12-31 23:28:51 +0200

    finished tag wise product now products returens when we click on tags

--- resources/views/frontend/common/vertical_menu.blade.php
+++ resources/views/frontend/common/vertical_menu.blade.php
@@ -1,10 +1,15 @@
 @php
+
 $categories = App\Models\Category::orderBy('category_name_en','ASC')->get();
 @endphp 
+
+
  <div class="side-menu animate-dropdown outer-bottom-xs">
           <div class="head"><i class="icon fa fa-align-justify fa-fw"></i> Categories</div>
           <nav class="yamm megamenu-horizontal">
             <ul class="nav">
+
+
               @foreach($categories as $category)
               <li class="dropdown menu-item"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon {{ $category->category_icon }}" aria-hidden="true"></i>
 @if(session()->get('language') == 'hindi') {{ $category->category_name_hin }} @else {{ $category->category_name_en }} @endif
@@ -12,10 +17,12 @@ $categories = App\Models\Category::orderBy('category_name_en','ASC')->get();
                 <ul class="dropdown-menu mega-menu">
                   <li class="yamm-content">
                     <div class="row">
+
    <!--   // Get SubCategory Table Data -->
   @php
   $subcategories = App\Models\SubCategory::where('category_id',$category->id)->orderBy('subcategory_name_en','ASC')->get();
   @endphp                   
+
              @foreach($subcategories as $subcategory)
                       <div class="col-sm-12 col-md-3">
  <h2 class="title">
@@ -26,6 +33,7 @@ $categories = App\Models\Category::orderBy('category_name_en','ASC')->get();
   @php
   $subsubcategories = App\Models\SubSubCategory::where('subcategory_id',$subcategory->id)->orderBy('subsubcategory_name_en','ASC')->get();
   @endphp                
+
    @foreach($subsubcategories as $subsubcategory)         
                         <ul class="links list-unstyled">
                           <li><a href="#">
@@ -33,6 +41,7 @@ $categories = App\Models\Category::orderBy('category_name_en','ASC')->get();
                           
                         </ul>
  @endforeach <!-- // End SubSubCategory Foreach -->
+
                       </div>
                       <!-- /.col -->
                  @endforeach  <!-- End SubCategory Foreach -->      
@@ -45,6 +54,8 @@ $categories = App\Models\Category::orderBy('category_name_en','ASC')->get();
                 <!-- /.dropdown-menu --> </li>
               <!-- /.menu-item -->
               @endforeach  <!-- End Category Foreach -->
+
+
  
               
               
