9384337f91ad9e2baec9424b1d1e1834e6a8fdfc
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2025-01-06 05:14:37 +0200

    finish wishlist function- video 350

--- routes/web.php
+++ routes/web.php
@@ -204,6 +204,16 @@ Route::get('/minicart/product-remove/{rowId}', [CartController::class, 'RemoveMi
 
 // Add to Wishlist
 Route::post('/add-to-wishlist/{product_id}', [CartController::class, 'AddToWishlist']);
+
+
+
+Route::group(['prefix'=>'user','middleware' => ['user','auth'],'namespace'=>'User'],function(){
+
+
+
 // Wishlist page
 Route::get('/wishlist', [WishlistController::class, 'ViewWishlist'])->name('wishlist');
 Route::get('/get-wishlist-product', [WishlistController::class, 'GetWishlistProduct']);
+Route::get('/wishlist-remove/{id}', [WishlistController::class, 'RemoveWishlistProduct']);
+
+});
